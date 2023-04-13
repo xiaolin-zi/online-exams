efa2b29e3e03f058bb0292a4a089f739b4e84e41
xiaolin <1849569695@qq.com>
2023-04-13 09:01:48 +0800

    测试服务器自动部署2

--- src/test/java/com/lxg/exams/OnlineExamsApplicationTests.java
+++ src/test/java/com/lxg/exams/OnlineExamsApplicationTests.java
@@ -8,7 +8,7 @@ class OnlineExamsApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!O(∩_∩)O哈哈~");
 
         }
 
